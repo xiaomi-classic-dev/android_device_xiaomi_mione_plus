0c0dbe9f241163c26754f064148bd719be79e633
Shantur Rathore <shantur.rathore@gmail.com>
2015-10-28 22:56:45 +0000

    mione: bluetooth: Disable BLE and deps as mione doesnt have it.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,5 +19,8 @@
 
 #define BTM_DEF_LOCAL_NAME   "Xiaomi MI-1"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 
 #endif
